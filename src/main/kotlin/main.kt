072ab97ec7398dcc4526500c22aa78f81164e035
sahactys <895004562613ds@gmail.com>
2022-03-13 13:35:43 +0300

    working with data types

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,23 +1,24 @@
 const val COMMISSION = 0.0075
-const val MINCOMMISSION:Double = 3500.0
+const val MIN_COMMISSION: Double = 3500.0
 
 fun main() {
-    var sum: Double
+    var amount: Double
     println("Введите сумму перевода")
     while (true) {
         try {
-            sum = (readLine()?.toDouble() ?: return) * 100
+            amount = (readLine()?.toDouble() ?: return) * 100
+            if (amount<0) error(0)
             break
         } catch (e: Exception) {
             println("Неверная сумма, попробуйте еще раз")
         }
     }
-    val sumCommission:Double = (if (sum * COMMISSION > MINCOMMISSION) sum * COMMISSION else MINCOMMISSION)
-    sum = (sum - sumCommission) / 100
-    println("Сумма комиссии = ${(sumCommission/100).format(2)} руб.")
-    println("Итоговая сумма перевода = ${sum.format(2)} руб.")
+    val sumCommission: Int = (if (amount * COMMISSION > MIN_COMMISSION) amount * COMMISSION else MIN_COMMISSION).toInt()
+    val totalAmount = (amount - sumCommission).toInt()
+    println("Сумма комиссии = " + sumCommission / 100 + "руб. " + sumCommission % 100 + "коп.")
+    println("Итоговая сумма перевода = " + totalAmount / 100 + "руб. " + totalAmount%100 + "коп.")
 }
 
-fun Double.format(digits: Int) = "%.${digits}f".format(this)
+
 
 
